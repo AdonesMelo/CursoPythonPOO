890bb4dedbc3e6057afe4d1ee857037794a5f816
Adones Melo <adones.n.m@outlook.com>
2026-07-07 12:33:18 -0300

    POO: Herança-ex006

---
+++ exercicios/ex006/__main__.py
@@ -0,0 +1,24 @@
+from rich import print,inspect
+from aluno import Aluno
+from professor import Professor
+from fucionario import Funcionario
+
+def main():
+    a1 = Aluno('José', 17, 'Informática', 'T01')
+    #print(a1.__dict__)
+    a1.fazer_matricula()
+    a1.fazer_aniversario()
+    #inspect(a1, methods=True)
+
+    p1 = Professor('Guanabarra', 42, 'TI', 'Mestrato')
+    p1.dar_aula()
+    p1.fazer_aniversario()
+    #inspect(p1, methods=True)
+
+    f1 = Funcionario('Maria', 25, 'Secretária', 'Secretaria')
+    f1.bater_ponto()
+    f1.fazer_aniversario()
+    #inspect(f1, methods=True)
+
+if __name__ == '__main__':
+    main()
